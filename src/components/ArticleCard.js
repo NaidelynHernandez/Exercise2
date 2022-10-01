7c9b5b0bcacba519f12c8016f4557d0ac2569b0a
Naidelyn Hernandez <naidelynhernandez@Naidelyns-MacBook-Pro.local>
2022-09-30 23:15:52 -0400

    added code to article card js

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,24 +1,31 @@
 
-import React, {useMemo}, 
+//import React, {useMemo}, 
 
-import React from "react";
+import React, {useMemo} from "react";
 
 
 function ArticleCard ({blurb, date,id,imageAlt,imageSrc, title}){
     const articleDate= useMemo (() => {
         if(!date)return '';
+        const parsedDate = new Date(date);
         return parsedDate.toDateString();
-    }, [articleData]);
+    }, [date]);
 
     
 
-    <p className="articleCard--text">
-        <h2>{title}</h2>
-
-        <p className= "link">
+    return(
+        <div className="articleCard">
+            <div className="articleCard--image">
+                <img src={imageSrc} alt={imageAlt}/>
+            </div>
+            <div className="articleCard--text">
+            <h2>{title}</h2>
+            <p className="date">{articleDate}</p>
+            <p className= "blurb"> {blurb}</p> 
+            <p className="link">
             <a href= {`/article/${id}`}> Read more</a></p>
             </div> 
-            </div> 
+        </div>
 
         );
     
